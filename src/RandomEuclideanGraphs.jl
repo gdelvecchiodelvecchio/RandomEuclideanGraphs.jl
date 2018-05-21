32344e4d6c630401578ce9b89db9e5fc31d00e22
Giuseppe Del Vecchio Del Vecchio <33101280+gdelvecchiodelvecchio@users.noreply.github.com>
2018-05-21 12:18:13 +0200

    Update RandomEuclideanGraphs.jl

--- src/RandomEuclideanGraphs.jl
+++ src/RandomEuclideanGraphs.jl
@@ -4,8 +4,11 @@ module RandomEuclideanGraphs
 
 
 include("BasicFunctions.jl")
-
-export pk
-
+#=
+export pk, euclidean_cost, euclidean_distance
+export pcost, nearest_neighbors_bipartite, nearest_neighbors_monopartite
+export nearest_neighbors_bipartite_inf, nearest_neighbors_monopartite_inf
+export randomVerteMap!, fillEdgeMap!, weight
+=#
 
 end
