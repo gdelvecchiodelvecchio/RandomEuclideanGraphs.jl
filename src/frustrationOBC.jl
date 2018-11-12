8c5ecf7a50aa0290f473969d8a5112040aa07a1c
Giuseppe Del Vecchio Del Vecchio <33101280+gdelvecchiodelvecchio@users.noreply.github.com>
2018-11-12 22:36:36 +0100

    Update frustrationOBC.jl

--- src/frustrationOBC.jl
+++ src/frustrationOBC.jl
@@ -164,7 +164,7 @@ function pk(G::AGraph, D::Int64, nI::Int64, P::Float64, f::UnionAll, param...)
 
     @inbounds  @fastmath for i in 1:n
         prob[i] = mean(probmat[:,i])
-        std1[i] = std(probmat[:,i]) / nInst
+        std1[i] = std(probmat[:,i]) / sqrt(nInst)
     end
 
     return prob, std1
